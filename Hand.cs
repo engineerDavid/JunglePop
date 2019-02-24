1312874a02287354edd47b9d2cd1bf961d7463e5
bigPappiDavid <33636080+bigPappiDavid@users.noreply.github.com>
2019-02-23 23:08:52 -0600

    Add files via upload

---
+++ Hand.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+
+    // Use this for initialization
+    public Transform mHandMesh;
+    
+
+    private void Update()
+    {
+        //helps make sure that the player does not see the mesh
+        //for testing it will be used
+        //after that then it will be taken off and set to false
+        //to activate hand mesh delete                                         mHandMesh.gameObject.SetActive(false);
+
+        //mHandMesh.gameObject.SetActive(false);
+        mHandMesh.position = Vector3.Lerp(mHandMesh.position, transform.position, Time.deltaTime * 15.0f);
+        
+        
+
+
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Bubble"))
+            return;
+
+
+
+
+        //sets it to false then sets it to postion 0,0,0 , but still moves
+        collision.gameObject.SetActive(false);
+
+
+        ScoreScript.scoreValue += 10;
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ test_back.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_back : MonoBehaviour
+{
+
+    float scrollSpeed = -10f;
+    Vector2 startPos;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        startPos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 130);
+        transform.position = startPos + Vector2.right * newPos;
+
+    }
+}
